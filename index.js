5d8d3e5deb029edd35fe8d27b557020a276646fb
Piet Geursen <pietgeursen@gmail.com>
2016-08-30 19:18:31 +1200

    add inux stuff

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+import { start, html, pull } from 'inu'
+import { App, Domain, Action } from 'inux'
 
+const view = (model, dispatch) => html` 
+<main>
+	<h1>Hello world</h1>	
+</main>
+`
+
+const home = Domain({
+  name: 'home',
+  init: () => ({
+    model: {}
+  }),
+  update: {},
+  routes: [
+    ['/', (params, model, dispatch) => view(model, dispatch)]
+  ]
+})
+
+const app = App([home])
+const sources = start(app)
+
+const main = document.querySelector('main')
+
+pull(
+  sources.views(),
+  pull.drain(function (view) {
+    html.update(main, view)
+  })
+)
