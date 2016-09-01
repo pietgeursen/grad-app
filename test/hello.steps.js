217de0623a32490cc49fed02a50e1d8ac7e7b2cb
Piet Geursen <pietgeursen@gmail.com>
2016-09-01 15:50:08 +1200

    Wooooo! Passing test with streaming element mutations!

--- test/hello.steps.js
+++ test/hello.steps.js
@@ -1,21 +1,30 @@
+var pull = require('pull-stream')
+var domMutant = require('pull-dom-mutants')
+
 module.exports = [
   [/^I am on the home page$/, function (t, world) {
     const main = window.document.createElement('main')
     window.document.body.appendChild(main)
+    world.main = main
     require('../index')
     world.window = window
     t.ok(true)
     t.end()
   }],
   [/^I should see "(.*)" as the page title$/, function (t, world, params) {
-    debugger
     const window = world.window
     const document = window.document
-    process.nextTick(function () {
-      const titleElem = document.querySelector('h1')
-      t.equal(titleElem.textContent, params[1])
-      t.end()
-      window.close()
-    })
+
+    pull(
+      domMutant(world.main),
+      pull.take(1),
+      pull.drain(function (mutation) {
+        const target = mutation.target
+        const titleElem = target.querySelector('h1')
+        t.equal(titleElem.textContent, params[1])
+        t.end()
+        window.close()
+      })
+    )
   }]
 ]
