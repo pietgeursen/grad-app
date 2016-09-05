53fc0e8ea8380a9d2312f1ed43a20624dbd22253
Piet Geursen <pietgeursen@gmail.com>
2016-09-05 22:27:07 +1200

    Rename test files for clarity.

--- test/cuke.js
+++ test/cuke.js
@@ -6,7 +6,7 @@ const cuke = require('cuke-tap')
 
 const helloFeatures = path.join(__dirname, 'hello.feature')
 const viewGradFeatures = path.join(__dirname, 'viewGrads.feature')
-const steps = require('./hello.steps.js')
+const steps = require('./steps.js')
 
 cuke({
   steps: steps,
