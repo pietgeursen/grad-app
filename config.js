a8aea22a124eac928052ca4dc3f7e4c18ae85aee
Piet Geursen <pietgeursen@gmail.com>
2016-09-14 10:30:03 +1200

    Add aws url.

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const config = {
 
 if (process.env.NODE_ENV === 'production') {
   config.port = process.env.PORT
-  config.host = '',
+  config.host = 'http://grad-app-server-dev.ap-southeast-2.elasticbeanstalk.com/',
   config.url = '//staging.holodex.is',
   config.letsencrypt = {
     path: join(__dirname, 'letsencrypt'),
