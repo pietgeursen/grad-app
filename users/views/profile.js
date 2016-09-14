b33cf38b7005ee8ed15e0b5bd164740a1125969c
Piet Geursen <pietgeursen@gmail.com>
2016-09-14 15:50:48 +1200

    Fix routing snafu.

--- users/views/profile.js
+++ users/views/profile.js
@@ -9,22 +9,23 @@ function user ({id}, model, dispatch) {
   })
   return html`
   <main>
-    <div class='grad'>
       ${ user ? 
-        html`<div class="grad-image">
-          <img src=${user.image_link} alt="image of ${user.name}">
+        html`
+        <div class='grad'>
+          <div class="grad-image">
+            <img src=${user.image_link} alt="image of ${user.name}">
+          </div>
+          <div>${user.name}</div>
+          <div>${user.long_description}</div>
+          <div>${user.phone}</div>
+          <div>${user.cv_link}</div>
+          <div>${user.github_link}</div>
+          <div>${user.linkedin_link}</div>
+          <div>${user.email}</div>
+          <button onclick=${() => dispatch(navigate(`/`))}>Back</button>
         </div>
-        <div>${user.name}</div>
-        <div>${user.long_description}</div>
-        <div>${user.phone}</div>
-        <div>${user.cv_link}</div>
-        <div>${user.github_link}</div>
-        <div>${user.linkedin_link}</div>
-        <div>${user.email}</div>
-        <button onclick=${() => dispatch(navigate(`/`))}>Back</button>
         `
        : html`<h2>We got no grads</h2>` }
-    </div>
   </main>
   `
 }
