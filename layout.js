f165a6c5918dd9829b5de5ebd340084114122e2c
Piet Geursen <pietgeursen@gmail.com>
2016-10-11 20:44:52 +1300

    Header and footer icons responsive enough.

--- layout.js
+++ layout.js
@@ -4,16 +4,15 @@ const { navigate } = require('inux')
 module.exports = (render) => (params, model, dispatch) => {
   return html`
     <main>
-      <div class="top-bar">
-        <div class="top-bar-left">
           <ul class="menu row align-justify">
-            <li class="menu-text column small-10">Enspiral Dev Academy</li>
+            <div class="small-2 columns">
+              <img src="/logo-02.svg" alt="Enspiral Dev Academy Logo"> 
+            </div> 
+            <li class="small columns"></li>
             <button class="column small-2" id="login" onclick=${() => dispatch(navigate('/login'))}>
               <i class="fi-torso"></i>
             </button>
           </ul>
-        </div>
-      </div>
     <div class="callout primary">
       <div class="row column text-center">
         <h1>Our Graduates</h1>
@@ -21,11 +20,12 @@ module.exports = (render) => (params, model, dispatch) => {
     </div>
       ${render(params, model, dispatch)}
       <footer>
-        <div class="row">
-          <div class="small columns"></div>
+        <div class="row expanded align-spaced">
+          <div class="small-6 columns"></div>
           <div class="small-2 columns">
             <img src="/logo-04.svg" alt="Enspiral Dev Academy Logo"> 
           </div> 
+          <div class="small-1 hide-for-large columns"></div> 
         </div> 
       </footer>
     </main>
