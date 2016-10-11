f689a4943e5b75c485c6f175a7429471e29e1b19
Piet Geursen <pietgeursen@gmail.com>
2016-10-11 22:32:17 +1300

    Top and bottom bars and icons are nice and responsive.

--- layout.js
+++ layout.js
@@ -4,28 +4,24 @@ const { navigate } = require('inux')
 module.exports = (render) => (params, model, dispatch) => {
   return html`
     <main>
-          <ul class="menu row align-justify">
-            <div class="small-2 columns" onclick=${() => dispatch(navigate('/'))}>
-              <img src="/logo-02.svg" alt="Enspiral Dev Academy Logo"> 
-            </div> 
-            <li class="small columns"></li>
-            <button class="column small-2" id="login" onclick=${() => dispatch(navigate('/login'))}>
-              <i class="fi-torso"></i>
-            </button>
-          </ul>
-    <div class="callout primary">
-      <div class="row column text-center">
-        <h1>Our Graduates</h1>
+      <div class="row align-justify top-bar expanded light-gray">
+        <button id="home-icon" class="small-2 columns" onclick=${() => dispatch(navigate('/'))}>
+          <img src="/logo-02.svg" alt="Enspiral Dev Academy Logo"> 
+        </button> 
+        <button class="column small-2" id="login" onclick=${() => dispatch(navigate('/login'))}>
+          <i class="fi-torso"></i>
+        </button>
       </div>
-    </div>
+      <div id="menubar" class="top-bar expanded">
+        <div class="row column text-center">
+        </div>
+      </div>
       ${render(params, model, dispatch)}
       <footer>
         <div class="row expanded align-spaced">
-          <div class="small-6 columns"></div>
-          <div class="small-2 columns">
+          <div class="small-3 small-offset-9 medium-1 medium-offset-11 small-collapse columns">
             <img src="/logo-04.svg" alt="Enspiral Dev Academy Logo"> 
           </div> 
-          <div class="small-1 hide-for-large columns"></div> 
         </div> 
       </footer>
     </main>
