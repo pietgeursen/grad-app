7407b9772b4618bb19df218622eacac10a63c2fe
Piet Geursen <pietgeursen@gmail.com>
2016-10-16 22:01:57 +1300

    Add grads to admin page.

--- user/views/dashboard.js
+++ user/views/dashboard.js
@@ -4,17 +4,18 @@ const { run, navigate } = require('inux')
 const { create } = require('../effects')
 
 const dashboard = ({id}, model, dispatch) => {
-  //three features. first is create an account.
-  //second is edit any account.
+  // ui confirms created grad
   //redirect when account not admin
   const user = model.user
+  const grads = model.grads.get('grads')
   const handleCreate = (ev) => {
     ev.preventDefault()
     const formData = getFormData(ev.target)
     dispatch(run(create(formData)))
   }
   return html`<div> 
-    <form id="add-grad" onsubmit=${handleCreate}>
+    <h1>Admin Dashboard</h1>
+    <form id="add-grad" class="admin-section" onsubmit=${handleCreate}>
       <h2>Create a new grad account:</h2>
       <fieldset>
         <label>email</label>
@@ -26,6 +27,17 @@ const dashboard = ({id}, model, dispatch) => {
       </fieldset>
       <input type='submit' value='Create new Grad' />
     </form>   
+    <div class="admin-section grads-list">
+      <h2>Grad accounts</h2>
+      ${grads.map((grad) => {
+        return html`
+        <div class="row">
+          <div class="small-10 columns">${grad.get('name')}</div> 
+          <button class="view-grad button small-2 columns" onclick=${() => dispatch(navigate(`grads/${grad.get('id')}/edit`))}>Edit</button>
+        </div>
+        `
+      }).toArray()}
+    </div>
   </div>`
 }
 
